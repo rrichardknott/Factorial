c37bebb0cf4bc9041ff914eaf95e8fb3997c47f4
rrichardknott <67433117+rrichardknott@users.noreply.github.com>
2020-07-13 07:31:43 -0700

    Initial commit

---
+++ Scripts/factorial.js
@@ -0,0 +1,27 @@
+﻿$("#errorAlert, #errorAlertButton").hide();
+
+$("#submitButton").on("click", function () {    
+           
+    let number = Number($("#number").val());           
+    if (isNaN(number) || $("#number").val() <=0 || $("#number").val().length == 0) {
+        $("#errorAlert, #errorAlertButton").show(1000);
+        $("#submitButton, #clearButton").hide();     
+    }
+    else {
+        let factorialResult = 1;
+        for (let i = 1; i <= number; i++) {
+            factorialResult = factorialResult * i;
+        }
+        $("#factorialNumber").val(factorialResult);          
+    }
+
+ });
+
+$("#errorAlertButton, #clearButton").on("click", function () {
+    $("#number, #factorialNumber").val("");
+    $("#errorAlert, #errorAlertButton").hide();
+    $("#submitButton, #clearButton").show();
+    $("#number").focus();
+});
+
+  
